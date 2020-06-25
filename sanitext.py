f97f15da65c7bd80d4e5fc045c012496fa7fef95
Anmol Pant <anmolpant@gmail.com>
2020-06-25 14:56:59 +0530

    icon pack

---
+++ sanitext.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font, colorchooser, filedialog, messagebox
+import os
+
+main_application = tk.Tk()
+main_application.geometry('1200x800')
+main_application.title('SaniText text editor')
+
+main_application.mainloop()
