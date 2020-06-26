6b0e9b9a0dd2b868520d9a85a3154d51281b7f19
Anmol Pant <anmolpant@gmail.com>
2020-06-26 11:40:37 +0530

    added file menu icons

--- sanitext.py
+++ sanitext.py
@@ -11,7 +11,16 @@ main_application.title('SaniText text editor')
 
 ###################################### main menu ########################################
 main_menu = tk.Menu()
+#file menu icons
+new_icon = tk.PhotoImage(file ='icons2/new.png')
+open_icon = tk.PhotoImage(file ='icons2/open.png')
+save_icon = tk.PhotoImage(file ='icons2/save.png')
+save_as_icon = tk.PhotoImage(file ='icons2/save_as.png')
+exit_icon = tk.PhotoImage(file ='icons2/exit.png')
 file = tk.Menu(main_menu, tearoff= False)
+
+file.add_command(label ="New", image=new_icon)
+
 edit = tk.Menu(main_menu, tearoff= False)
 view = tk.Menu(main_menu, tearoff= False)
 color_theme = tk.Menu(main_menu, tearoff= False)
