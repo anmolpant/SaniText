8a6382c68350783821d62ba26879f3e0671eb826
Anmol Pant <anmolpant@gmail.com>
2020-07-13 19:27:31 +0530

    added .dll files and setup.py paths

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ base = None
 if sys.platform == 'win32':
     base = "Win32GUI"
 
-os.environ['TCL_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python37\tcl\tcl8.6"
-os.environ['TK_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python37\tcl\tk8.6"
+os.environ['TCL_LIBRARY'] = r"C:\Users\anmol\AppData\Local\Programs\Python\Python37-32\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\anmol\AppData\Local\Programs\Python\Python37-32\tcl\tk8.6"
 
 executables = [cx_Freeze.Executable("vpad.py", base=base, icon="icon.ico")]
 
 
 cx_Freeze.setup(
-    name = "Vpad Text Editor",
+    name = "SaniText Text Editor",
     options = {"build_exe": {"packages":["tkinter","os"], "include_files":["icon.ico",'tcl86t.dll','tk86t.dll', 'icons2']}},
     version = "0.01",
     description = "Tkinter Application",
